b8cd08b67809216f8f3d4de5af01c6807b0ef9dc
SomeRandomGuy45 <143886716+SomeRandomGuy45@users.noreply.github.com>
2023-10-23 22:11:31 -0500

    Rewritten code

--- source.cpp
+++ source.cpp
@@ -13,57 +13,43 @@
 #endif
 
 using namespace std;
-bool isGameRunning;
 
-// a simple quiz game
-void MakeQuizStuff(int QNum, int Answer, string QName, string Q1, string Q2, string Q3, string Q4) {
-    if (isGameRunning == true) {
-        if (Answer > 4) {
-            cout << "Error: Answer cannot be above four!";
-        } else {
-            cout << "Question " << QNum << ". " << QName << endl;
-            cout << "1. " << Q1 << "\t";
-            cout << "2. " << Q2 << "\n";
-            cout << "3. " << Q3 << "\t";
-            cout << "4. " << Q4 << "\n";
-            int AnswerInput;
-            cout << "What is the answer? ";
-            cin >> AnswerInput;
-            if (AnswerInput == Answer) {
-                cout << "GOOD JOB!!!!!!!\n";
-                sleep(5);
-                #if defined(__linux__)
-                    system("clear");
-                #elif _WIN32
-                    system("cls");
-                #elif __APPLE__
-                    system("clear");
-                #endif // defined
-            } else {
-                cout << "WRONG ANSWER!!!!\n";
-                sleep(5);
-                #if defined(__linux__)
-                    system("clear");
-                #elif _WIN32
-                    system("cls");
-                #elif __APPLE__
-                    system("clear");
-                #endif // defined
-            }
-        }
-    } else {
-        cout << "Error: Game is not running!";
+void MakeQuiz(int Num, int Ans, string QN, string Q1, string Q2, string Q3, string Q4)
+{
+    cout << "Question" << Num << ". " << QN << endl;
+    cout << "1. " << Q1 << endl;
+    cout << "2. " << Q2 << endl;
+    cout << "3. " << Q3 << endl;
+    cout << "4. " << Q4 << endl;
+    cout << "What is the answer? ";
+    int input;
+    cin >> input;
+    if (input == Ans)
+    {
+        cout << "You are right!";
+        sleep(2.5);
+        #if defined(__linux__)
+            system("clr");
+        #elif _WIN32
+            system("clear");
+        #elif __APPLE__
+            system("clr");
+        #endif
+    } else if (input != Ans) {
+        cout << "You are wrong :(";
+        sleep(2.5);
+        #if defined(__linux__)
+            system("clr");
+        #elif _WIN32
+            system("clear");
+        #elif __APPLE__
+            system("clr");
+        #endif
     }
 }
 
-
-int main() {
-    if (isGameRunning == true) {
-        cout << "ERROR: Game is already running!";
-        return 5;
-    } else {
-        isGameRunning = true;
-        MakeQuizStuff(1, 4, "life", "is", "roblox", "fr", "fr");
-    }
+int main()
+{
+    MakeQuiz(1, 2, "What is the thing", "What?", "Life is roblox", "Let's go golfing", "Yeah no");
     return 0;
 }
